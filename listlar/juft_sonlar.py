60976f3f9261ffbc9c8e689c5431dcb435edb100
AAmir007-code <amiraslonov00@gmail.com>
2022-07-06 06:32:27 -0400

    juft sonlarni topish 2

--- listlar/juft_sonlar.py
+++ listlar/juft_sonlar.py
@@ -1,9 +1,2 @@
-# input list elements
-l = []
-for i in input().split():
-	l.append(int(i))
-even_nums = []
-for i in range(len(l)):
-	if l[i]%2 == 0:
-		even_nums.append(l[i])
-print('Even nums => ',even_nums)
+# one line
+print('Even nums => ',list(map(int,filter(lambda x: int(x)%2==0,input().split()))))
